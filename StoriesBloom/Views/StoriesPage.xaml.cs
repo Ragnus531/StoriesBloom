05bc1ec854f66b2dabe1e51bafccc2dca2a6d936
Ragnus531 <jakub8327@gmail.com>
2023-11-11 20:35:06 +0100

    Stories Detail Page

--- StoriesBloom/Views/StoriesPage.xaml.cs
+++ StoriesBloom/Views/StoriesPage.xaml.cs
@@ -20,8 +20,16 @@ public partial class StoriesPage : ContentPage
 
     private void picker_SelectedIndexChanged(object sender, EventArgs e)
     {
-		flexLayout.Clear();
-		flexLayout.Add(new Label() { Text = "nnn" }); 
-		flexLayout.Add(new Label() { Text = "nnn222" });
+		//flexLayout.Clear();
+		//flexLayout.Add(new Label() { Text = "nnn" }); 
+		//flexLayout.Add(new Label() { Text = "nnn222" });
+    }
+
+    private async void CollectionView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        await Shell.Current.GoToAsync(nameof(StoriesDetailPage), true, new Dictionary<string, object>
+        {
+            { "Item", new StoryDetail("f","f","f","f","f","f","f","f","d") }
+        });
     }
 }
