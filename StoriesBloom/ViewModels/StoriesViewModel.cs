5547c635c4c3717fb0fff1e6ab7b93bec3b50eae
Ragnus531 <jakub8327@gmail.com>
2023-11-12 14:47:04 +0100

    #6 - Init list of novels

--- StoriesBloom/ViewModels/StoriesViewModel.cs
+++ StoriesBloom/ViewModels/StoriesViewModel.cs
@@ -1,4 +1,7 @@
-﻿namespace StoriesBloom.ViewModels;
+﻿using CommunityToolkit.Mvvm.Input;
+using System.Windows.Input;
+
+namespace StoriesBloom.ViewModels;
 
 public partial class StoriesViewModel : BaseViewModel
 {
@@ -13,12 +16,17 @@ public partial class StoriesViewModel : BaseViewModel
 	[ObservableProperty]
 	ObservableCollection<StoryDetail> items;
 
-	public StoriesViewModel(StoryDataService service)
+    public ICommand IncrementCounterCommand { get; }
+    public ICommand GoToNovelDetailsCommand { get; }
+
+    public StoriesViewModel(StoryDataService service)
 	{
 		dataService = service;
-	}
+        IncrementCounterCommand = new RelayCommand(ChangeElements);
+        GoToNovelDetailsCommand = new AsyncRelayCommand(GoToDetail);
+    }
 
-	[RelayCommand]
+    [RelayCommand]
 	private async void OnRefreshing()
 	{
 		IsRefreshing = true;
@@ -58,9 +66,18 @@ public partial class StoriesViewModel : BaseViewModel
 		});
 	}
 
-	[RelayCommand]
-	private async void ChangeElements(string category)
+	private void ChangeElements()
 	{
+		var mm = Category;
+		//Change elements to specific category
 		Console.WriteLine("dasds");
 	}
+
+	private async Task GoToDetail()
+	{
+        await Shell.Current.GoToAsync(nameof(StoriesDetailPage), true, new Dictionary<string, object>
+        {
+            { "Item", new StoryDetail("f","f","f","f","f","f","f","f","d") }
+        });
+    }
 }
