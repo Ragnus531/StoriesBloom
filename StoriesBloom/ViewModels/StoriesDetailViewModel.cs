29ad479e9ba232d5301af49ae1de6c94871c4a6a
Ragnus531 <jakub8327@gmail.com>
2023-11-24 22:59:19 +0100

    Go Back button

--- StoriesBloom/ViewModels/StoriesDetailViewModel.cs
+++ StoriesBloom/ViewModels/StoriesDetailViewModel.cs
@@ -1,8 +1,23 @@
-﻿namespace StoriesBloom.ViewModels;
+﻿using CommunityToolkit.Mvvm.Input;
+using System.Windows.Input;
+
+namespace StoriesBloom.ViewModels;
 
 [QueryProperty(nameof(Item), "Item")]
 public partial class StoriesDetailViewModel : BaseViewModel
 {
 	[ObservableProperty]
     StoryDetail item;
+
+    public ICommand GoBackCommand { get; }
+
+	public StoriesDetailViewModel()
+	{
+        GoBackCommand = new AsyncRelayCommand(GoBack);
+	}
+
+    private async Task GoBack()
+    {
+        await Shell.Current.GoToAsync("..");
+    }
 }
